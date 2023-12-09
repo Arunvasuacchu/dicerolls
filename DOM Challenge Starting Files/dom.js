16e368d16be24b5d5426a920dd2ee4a83885636b
Arunvasuacchu <arunvasur@gmail.com>
2023-12-09 17:04:37 +0530

    dicerollmade

---
+++ DOM Challenge Starting Files/dom.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btn")
+const hello = document.getElementById("hello")
+function color(){
+  btn.textContent = "Changed"
+  btn.style.backgroundColor = "yellow"
+  hello.innerHTML = "<p> arunvasu <p>"
+}
+
+
+
